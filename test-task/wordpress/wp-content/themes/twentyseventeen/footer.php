f1d5f0b0bed86e43423dd8e4be33a6d180c87000
nickba69 <illiterateone@yandex.ru>
2017-02-02 14:58:27 +0200

    test

---
+++ test-task/wordpress/wp-content/themes/twentyseventeen/footer.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+		</div><!-- #content -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="wrap">
+				<?php
+				get_template_part( 'template-parts/footer/footer', 'widgets' );
+
+				if ( has_nav_menu( 'social' ) ) : ?>
+					<nav class="social-navigation" role="navigation" aria-label="<?php _e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'social',
+								'menu_class'     => 'social-links-menu',
+								'depth'          => 1,
+								'link_before'    => '<span class="screen-reader-text">',
+								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
+							) );
+						?>
+					</nav><!-- .social-navigation -->
+				<?php endif;
+				?>
+				<div class="contact-us">
+					<p class="mobile-hide">Call Us: 0987654321</p>
+                    	<button class="mobile-button">
+                    		Call Us
+                    	</button>
+                	<p class="mobile-hide"> Email:   
+                 		<a href="mailto:testdomain@mail.to">testdomain@mail.to</a>
+                 	</p>
+	               		<button class="mobile-button">Email
+	               			<a href="mailto:testdomain@mail.to"></a>
+	               		</button>
+               		<p>
+	                	<button class="caddy">Contact Us</button>
+	                </p>
+
+	                <span class="popup">+ 4545 4545 56565      ninja 
+	                </span>
+				</div>
+
+				
+
+
+
+				<?php
+				get_template_part( 'template-parts/footer/site', 'info' );
+				?>
+				
+                
+			</div><!-- .wrap -->
+		</footer><!-- #colophon -->
+	</div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
